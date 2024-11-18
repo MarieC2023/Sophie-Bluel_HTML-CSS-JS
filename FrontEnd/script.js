34033968a239dc09847a44b84ae84d979f39f6ba
Marie COLLET <marie.collet2023@gmail.com>
2024-11-18 11:29:52 +0100

    Récupération des travaux depuis le back-end

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,8 +1,23 @@
-let a = fetch("http://localhost:5678/api/works")
+let works = fetch("http://localhost:5678/api/works")
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        let display = ""
+        //console.log(data)
+        for(let figure of data){
+            display += `
+            <figure "${figure.id}">
+                  <img src="${figure.imageUrl}" alt="${figure.title}">
+                  <figcaption>${figure.title}</figcaption>
+              </figure>
+         `
+         //console.log(figure)
+        }
+        //console.log(display)
+        document.querySelector(".gallery").innerHTML = display
+})
+
     .catch(err => {
         console.log("dans le catch")
         console.log(err)
     })
-console.log (a)
+console.log (works)
