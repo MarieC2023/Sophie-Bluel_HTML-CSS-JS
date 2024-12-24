5b9b224fe94bcbd3e2d7cc37b9cc4341d0804d7e
Marie COLLET <marie.collet2023@gmail.com>
2024-12-24 11:59:10 +0100

    Création fonction APIDelete

--- FrontEnd/assets/script/API.js
+++ FrontEnd/assets/script/API.js
@@ -25,7 +25,8 @@ export const APIWorks = async () => {
     ////////////////////////////
 
 export const APIConnection = async ({email, password}) => {
-    const response = await fetch (`${URL}/users/login`, {
+    try{
+        const response = await fetch (`${URL}/users/login`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -33,5 +34,40 @@ export const APIConnection = async ({email, password}) => {
         body: JSON.stringify({email, password})
     });
     if (!response.ok) throw new Error(`Erreur HTTP : ${response.status} ${response.statusText}`);
-    return response.json();
+    const data = await response.json();            
+    return data;
+    } catch (err) {
+        console.log ("Erreur lors de la connection : ", err.message);
+        throw err;
+    }
 }; 
+
+    //////////////////////////////////
+    ///// Supression des oeuvres /////
+    //////////////////////////////////
+
+export const APIDelete = async (figureId, userToken) => {
+    try {
+        const response = await fetch(`${URL}/${figureId}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${userToken}`,
+            },
+
+        });
+        if (response.ok) {
+            console.log(`Image ${figureId} supprimée avec succès.`);
+            const modalFigure = document.querySelector(`#modal-figure-${figureId}`).remove();
+            console.log(modalFigure)
+        } else {
+            console.error(`Erreur lors de la suppression : ${response.status} ${response.statusText}`);
+        }
+    } catch (err) {
+        console.error("Erreur API lors de la suppression :", err);
+    }
+
+}; 
+
+    ///////////////////////////////////////
+    ///// Récupération des catégories /////
+    ///////////////////////////////////////
